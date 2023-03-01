0f32f0cad08aae2191a47a841f9f1edd783f8999
armullen <amber.r.mullen@gmail.com>
2023-02-28 23:50:34 -0600

    Working out array sequence

--- app.js
+++ app.js
@@ -8,19 +8,54 @@ const yellowBtn = document.querySelector('.yellow');
 const startBtn = document.querySelector('#start');
 let streakCounter = document.querySelector('#longestStreak');
 let gameStart = true;
-//Now I want to create an array of colors to iterate through to create patterns for the computer to flash.
 
-const colorArr = [redBtn, blueBtn, greenBtn, yellowBtn];
 
 //console.log(colorArr); expect array to print
 //I want a function that changes the color of the boxes to white for a second when the box is selected
 
-
-function colorSignal(){
+function colorSignalRed(){
     redBtn.classList.add('flash')
-    setTimeout(() => { redBtn.classList.remove('flash')}, (500))
+    setTimeout(() => { redBtn.classList.remove('flash')}, (250));
+}
+function colorSignalGreen(){
+    greenBtn.classList.add('flash')
+    setTimeout(() => { greenBtn.classList.remove('flash')}, (250));
+}
+function colorSignalYellow(){
+    yellowBtn.classList.add('flash')
+    setTimeout(() => { yellowBtn.classList.remove('flash')}, (250));
+}
+function colorSignalBlue(){
+    blueBtn.classList.add('flash')
+    setTimeout(() => { blueBtn.classList.remove('flash')}, (250));
 };
-redBtn.addEventListener('click', colorSignal);
+redBtn.addEventListener('click', colorSignalRed);
+greenBtn.addEventListener('click', colorSignalGreen);
+yellowBtn.addEventListener('click', colorSignalYellow);
+blueBtn.addEventListener('click', colorSignalBlue);
 
+//I want to create an array of colors to iterate through to create patterns for the computer to flash.
 
+//I want a function that predicts the array of 20 indicies of color in random order to pre load the game.
+const buttonArr = [redBtn, blueBtn, greenBtn, yellowBtn];
+let gameArray = buttonArr[Math.floor(Math.random() * buttonArr.length)];  //empty array to push game into.
 
+function gameSequence(){
+if(gameArray === '.red'){
+    colorSignalRed;
+} else{
+    if(gameArray === '.blue'){
+        colorSignalBlue
+    }else{
+        if(gameArray === '.green'){
+            colorSignalGreen
+        }else{
+            if(gameArray === '.yellow'){
+                colorSignalYellow
+            }
+        }
+    }
+}
+};
+    
+console.log(gameArray[i]);
