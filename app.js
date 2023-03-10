2b4eb1bd929c35c6b2005547dad9a3db92a3ee95
armullen <amber.r.mullen@gmail.com>
2023-03-09 21:02:47 -0600

    Fully functional game!

--- app.js
+++ app.js
@@ -12,15 +12,42 @@ const buttonArr = [redBtn, blueBtn, greenBtn, yellowBtn];
 //global variables
 let gameArr = [];
 let playerArr = [];
-let gameStart = false;
-let computerTurn = true;
-let round = 0;
+let round = 1;
 redBtn.disabled = true;
 greenBtn.disabled = true;
 blueBtn.disabled = true;
 yellowBtn.disabled = true;
 
 messageBox.innerHTML = 'Press start to play!'
+
+
+redBtn.addEventListener('click', colorSignalRed);
+greenBtn.addEventListener('click', colorSignalGreen);
+yellowBtn.addEventListener('click', colorSignalYellow);
+blueBtn.addEventListener('click', colorSignalBlue);
+
+
+redBtn.addEventListener('click', playerTurn);
+greenBtn.addEventListener('click', playerTurn);
+yellowBtn.addEventListener('click', playerTurn);
+blueBtn.addEventListener('click', playerTurn);
+
+
+startBtn.addEventListener('click', handleStartRound);
+
+function disableButtons() {
+        redBtn.disabled = true;
+        greenBtn.disabled = true;
+        blueBtn.disabled = true;
+        yellowBtn.disabled = true;
+}
+
+function enableButtons() {
+        redBtn.disabled = false;
+        greenBtn.disabled = false;
+        blueBtn.disabled = false;
+        yellowBtn.disabled = false;
+}
 
 
 function colorSignalRed() {
@@ -68,47 +95,6 @@ function colorSignalBlue() {
     })
 };
 
-
-redBtn.addEventListener('click', colorSignalRed);
-greenBtn.addEventListener('click', colorSignalGreen);
-yellowBtn.addEventListener('click', colorSignalYellow);
-blueBtn.addEventListener('click', colorSignalBlue);
-
-
-redBtn.addEventListener('click', playerTurn);
-greenBtn.addEventListener('click', playerTurn);
-yellowBtn.addEventListener('click', playerTurn);
-blueBtn.addEventListener('click', playerTurn);
-
-
-startBtn.addEventListener('click', handleStartRound);
-
-
-function handleStartRound() {
-    randomColorSelector();
-    showGameArr();
-    gameStart = true;
-    computerTurn = true;
-    enableButtons();
-}
-
-function roundCounter() {
-    if (round === 10) {
-        gameOver();
-    } else {
-        round++;
-        messageBox.innerHTML = `Round: ${round}`;
-        console.log('Round', round);
-    }
-}
-
-function randomColorSelector() {
-    let selectBtn = buttonArr[Math.floor(Math.random() * buttonArr.length)];
-    gameArr.push(selectBtn);
-    roundCounter();
-    console.log('Simons Array: ', gameArr);
-}
-
 async function showGameArr() {
     await new Promise(resolve => {
         setTimeout(() => resolve(), 1000);
@@ -126,62 +112,71 @@ async function showGameArr() {
     }
 }
 
-function disableButtons() {
-    if (!gameStart) {
-        redBtn.disabled = true;
-        greenBtn.disabled = true;
-        blueBtn.disabled = true;
-        yellowBtn.disabled = true;
+function handleStartRound() {
+    messageBox.innerHTML = `Round: ${round}`;
+    randomColorSelector();
+    showGameArr();
+    enableButtons();
+}
+
+function roundCounter() {
+    if (round === 10) {
+        beatSimon();
+    } else {
+        round++;
+        messageBox.innerHTML = `Round: ${round}`;
+        console.log('Round', round);
+        handleStartRound();  
     }
 }
 
-function enableButtons() {
-    if (gameStart) {
-        redBtn.disabled = false;
-        greenBtn.disabled = false;
-        blueBtn.disabled = false;
-        yellowBtn.disabled = false;
-    }
+function randomColorSelector() {
+    let selectBtn = buttonArr[Math.floor(Math.random() * buttonArr.length)];
+    gameArr.push(selectBtn);
+    console.log('Simons Array: ', gameArr);
 }
+
 
 function playerTurn() {
     playerArr.push(this);
     console.log('Player Array = ', playerArr);
+    if (playerArr.length === round) {
     checkAnswer();
-}
-
-
-function gameOver() {
-    if (round === 10) {
-        console.log('You Win! You beat Simon!')
-        messageBox.innerHTML = 'You beat Simon! Good job! Push start to try again.';
-    } else {
-        console.log('Game Over!')
-        messageBox.innerHTML = 'Game Over! Push start to try again.';
     }
-    round = 0;
-    gameArr = [];
-    playerArr = [];
-    gameStart = false;
-    disableButtons();
 }
-
 
 function checkAnswer() {
-    if (playerArr.length === round) {
-        for (let i = 0; i < playerArr.length; i++) {
-            if (gameArr[i] === playerArr[i]) {
-                console.log('Matched');
-            } else {
-                if (gameArr[i] !== playerArr[i]) {
-                    gameOver();
-                }
-            }
+    for (let i = 0; i < playerArr.length; i++) {
+        if (gameArr[i] === playerArr[i]) {
+            console.log('Matched');
+        } else {
+            gameOver();
+            return
         }
-        handleStartRound();
-        playerArr = [];
     }
+    playerArr = [];
+    roundCounter();
 }
+    
+    
+    function gameOver() {
+        console.log('Game Over!');
+        messageBox.innerHTML = 'Game Over! Push start to try again.';
+        round = 1;
+        gameArr = [];
+        playerArr = [];
+        disableButtons();
+    }
+    
+    function beatSimon() {
+        console.log('You Win! You beat Simon!');
+        messageBox.innerHTML = 'You beat Simon! Good job! Push start to try again.';
+        round = 1;
+        gameArr = [];
+        playerArr = [];
+        disableButtons();
+    }
+    
 
 
 
